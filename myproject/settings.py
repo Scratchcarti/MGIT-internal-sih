3babb2c1329ce9054b9382725316427cf5814686
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-09-07 20:19:24 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -130,3 +130,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'shaikjunaid124@gmail.com'
+EMAIL_HOST_PASSWORD = 'bcer pxeg xnes tjhj'
+
